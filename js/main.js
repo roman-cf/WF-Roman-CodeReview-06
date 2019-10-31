c030c39e62b5502797041a4b6f65bf2d6e3adf4f
roman.cf <roman.springer@outlook.com>
2019-10-31 09:12:50 +0100

    still at begining with the .ts

---
+++ js/main.js
@@ -0,0 +1,24 @@
+siteBuilder();
+
+//-------------------------------------------Site Builder---------------------------------------------------
+function siteBuilder(){
+	$("title").text("Travel blog");
+	$("body").append(`
+					<header id="header" class=" d-flex justify-content-between text-white  bg-primary p-2">
+						<p class="h2 m-1">My Travelblog</p>
+						<nav class=" navbar navbar-expand-md p-2 justify-content-end">
+							<button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarContent"
+							    aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon text-white" >&#8801;</span>
+							</button>
+							<div class="collapse navbar-collapse "  id="navbarContent">
+								<ul class="navbar-nav justify-content-end">
+								</ul>
+							</div>
+						</nav>	
+					</header>
+		`);
+	$("body").append(`<main><div id="maincontent" class="container"></div></main>`);
+	$("body").append(`<footer>&copy;</footer>`);
+}
+//---------------------------------------End Site Builder---------------------------------------------------
