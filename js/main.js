f09c3c8d1606709c0afe2e3ea75a219cd113c7c8
roman-cf <roman.springer@outlook.com>
2019-11-05 12:11:24 +0100

    new form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-var typeArray = [["loc","res","eve"],["Location only","Restaurants","Events"]];
+var typeArray = [["loc","res","eve"],["Location only","Restaurants","Events"],["MyLocation","myRestaurant","myEvent"]];
+var argumentsArr = [["lId","lName","lCity","lZip","lAdress","lPicUrl"],["id", "Location Name", "City", "Zip Code", "Adress", "Picture URL"]];
+var argumentsArrres= [["rType","rPhone","rWww"],["Restaurant Typ", "Phone", "www"]];
+var argumentsArreve=[["eDate","ePrice"],["Dec 31, 2019 23:59:59","Price for Entrance"]];
+var addId="";
 
 siteBuilder();
 addPlaces();
@@ -33,59 +37,60 @@ function siteBuilder(){
 
 //---------------------------------------Adding Places---------------------------------------------------
 function addPlaces(){
-	$("#maincontent").append(`<form class=" container ">
-							<h3> add a new Place</h3>
-							<ul class="addLocList nav nav-tabs"></ul>
-
-						  <div class="form-group">
-						    <input type="text" class="form-control" id="inF1" placeholder="Location Name">
-						  </div>
-						  <div class="form-group">
-						    <input type="text" class="form-control" id="inF2" placeholder="City">
-						  </div>
-						  <div class="form-group">
-						    <input type="text" class="form-control" id="inF3" placeholder="Zip Code">
-						  </div>
-						  <div class="form-group">
-						    <input type="text" class="form-control" id="inF4" placeholder="Adress">
-						  </div>
-						  <div class="form-group">
-						    <input type="text" class="form-control" id="inF5" placeholder="Picture URL">
-						  </div>
-
-						<div  id="loc" class="tab-content">
-							<div id="eve" class="tab-pane fade"">	
-								<div class="form-group">
-							   		<input type="text" class="form-control" id="inF6" placeholder="Date">
-								</div>
-								<div class="form-group">
-					   				<input type="text" class="form-control" id="inF7" placeholder="Price">
-								</div>
-							</div>
-							<div id="res" class="tab-pane fade"">	
-								<div class="form-group">
-							   		<input type="text" class="form-control" id="inF6" placeholder="Restauranttype">
-								</div>
-								<div class="form-group">
-					   				<input type="text" class="form-control" id="inF7" placeholder="www">
-								</div>
-							</div>
-						</div>
-
-							<p class="newBtn btn btn-primary">eintragen</p>
-
-						</form>
+	$("#maincontent").append(`
+					<form class=" container ">
+						<h3> add a new Place</h3>
+						<ul class="addLocList nav nav-tabs"></ul>
+						<div class="tab-content"></div>
+						<p class="newBtn btn btn-primary">eintragen</p>
+					</form>
 	`);
+	//-----------add tabs for input-------------------------
+	
+	for(i=0; i<typeArray[0].length; i++){
+		$(".addLocList").append(`<li><p class=" btn btn-light" data-toggle="tab" href="#`+typeArray[0][i]+`">`+typeArray[1][i]+`</p></li>`);
+	};	
 
+
+	$(".addLocList p").click(function(e){
+		addId=$(this).attr("href").slice(1);
+		$(".tab-content").empty();
+		$(".tab-content").append(`
+				<div id="`+addId+`" class="tab-pane fade"">
+				</div>
+		`);
+		for (let i =1; i<argumentsArr[0].length;i++){
+			$(".tab-pane ").append(`
+				<div class="form-group">
+					    <input type="text" class="form-control" id="inF`+i+`" placeholder="`+argumentsArr[1][i]+`">
+				</div>
+			`);
+		};
+		if(addId!="loc"){
+			for (let i =0; i<window["argumentsArr"+addId][0].length;i++){
+				console.log(argumentsArr[0].length + i)
+				$(".tab-pane ").append(`
+					<div class="form-group">
+						    <input type="text" class="form-control" id="inF`+(argumentsArr[0].length+i)+`" placeholder="`+window["argumentsArr"+addId][1][i]+`">
+					</div>
+				`);	
+			};
+		};
+	});
+
+	//-------------------------------------------
 	$(".newBtn").on("click",function(e){
-		var blogLocationX = new MyLocation(locationArray.length+1,$("#inF1").val() , $("#inF2").val() , $("#inF3").val() , $("#inF4").val() , $("#inF5").val());
+
+		var classConstr = typeArray[2][positionFinder(typeArray,addId)];
+		console.log(classConstr)
+
+		var blogLocationX = new MyLocation(addId+(locationArray.length+1),$("#inF1").val() , $("#inF2").val() , $("#inF3").val() , $("#inF4").val() , $("#inF5").val());
 		locationArray.push(blogLocationX);
 		$(".form-control").val("");
 		listLocation();
 	});
-	for(i=0; i<typeArray[0].length; i++){
-	$(".addLocList").append(`<li><p class="btn btn-light" data-toggle="tab" href="#`+typeArray[0][i]+`">`+typeArray[1][i]+`</p></li>`);
-	}
+
+
 };
 
 //---------------------------------------list all Locations--------------------------------------------------
@@ -103,3 +108,20 @@ function listLocation(){
 		}
 	}
 }
+//--------------------------
+function positionFinder(obj, value) {
+    for(var i = 0; i < obj.length; i ++) {
+        if(obj[0][i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+//----------------testarea---------
+	dumpArguments(MyLocation);	
+	function dumpArguments(...args) {
+	  for (var i = 0; i < args.length; i++)
+	    console.log(args[i]);
+	  return args;
+	}
+    //$("body").append('<ul>' + args + '</ul>');
